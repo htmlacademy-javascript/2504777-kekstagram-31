3107561eeee9202111077cb42cfe79f9fd8f3157
OksKuskova <oskanchik.kuskova@gmail.com>
2024-02-21 11:28:55 +0100

    Добавит и подключит functions.js

---
+++ js/functions.js
@@ -0,0 +1,42 @@
+// Задача 1
+const isStringLengthCorrect = (string, maxLength) => string.length <= maxLength;
+
+// Задача 2, вариант 1
+const isPolindrome = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  let reverseString = '';
+
+  for (let i = 1; i <= string.length; i++) {
+    reverseString += string.at(-i);
+  }
+  return string === reverseString;
+};
+
+// Задача 2, вариант 2
+const isPolindrome2 = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Задача 3
+const extractNumber = (text) => {
+  text = String(text);
+
+  let number = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const currentSymbol = text[i];
+    if (Number.isNaN(parseInt(currentSymbol, 10))) {
+      continue;
+    }
+    number += currentSymbol;
+  }
+  return parseInt(number, 10);
+};
