653089076809ff7a9883a5adb856b345d44c9a22
OksKuskova <oskanchik.kuskova@gmail.com>
2024-02-29 16:45:21 +0100

    Задача: Делу-время

--- js/functions.js
+++ js/functions.js
@@ -40,3 +40,41 @@ const extractNumber = (text) => {
   }
   return parseInt(number, 10);
 };
+
+// * Molule5-task2
+
+const MINUTES_IN_HOUR = 60;
+
+// 2.1
+const isKeepingUpWorkingHours = (startWorkingDay = '', endWorkingDay = '', startMeeting = '', durationMeeting = 0) => {
+
+  const data = [startWorkingDay, endWorkingDay, startMeeting].map((element) => element.split(':'));
+  const dataInMinutes = data.map((element) => element[0] * MINUTES_IN_HOUR + Number(element[1]));
+
+  ([startWorkingDay, endWorkingDay, startMeeting] = dataInMinutes);
+
+  const meetingEndTime = startMeeting + durationMeeting;
+
+  return startMeeting >= startWorkingDay && meetingEndTime <= endWorkingDay;
+};
+
+console.log(isKeepingUpWorkingHours('08:00', '17:30', '14:00', 90)); //eslint-disable-line
+
+// 2.2
+const convertTimeToMinutes = (time) => {
+  const data = time.split(':');
+  return data[0] * MINUTES_IN_HOUR + Number(data[1]);
+};
+
+const isKeepingUpWorkingHours2 = (startWorkingDay = '', endWorkingDay = '', startMeeting = '', durationMeeting = 0) => {
+  startWorkingDay = convertTimeToMinutes(startWorkingDay);
+  endWorkingDay = convertTimeToMinutes(endWorkingDay);
+  startMeeting = convertTimeToMinutes(startMeeting);
+
+  const meetingEndTime = startMeeting + durationMeeting;
+
+  return startMeeting >= startWorkingDay && meetingEndTime <= endWorkingDay;
+};
+
+console.log(isKeepingUpWorkingHours2('08:00', '14:30', '14:00', 90)); //eslint-disable-line
+
