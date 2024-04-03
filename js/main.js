0b78956a66d4e44d44d97ec6abf2e49b3d6a35ab
OksKuskova <oskanchik.kuskova@gmail.com>
2024-04-03 14:08:55 +0200

    добавит универсальную show-toast-error вместо get-data-error

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { renderThumbnails } from './thumbnails/render-thumbnails.js';
 import { savePostedPhotos } from './posted-photo-gallery.js';
-import { showDataErrorMessage } from './alerts/get-data-error.js';
+import { showToastError } from './alerts/show-toast-error.js';
 import { getData } from './api.js';
 import { initImagePopup } from './popup/init-image-popup.js';
 import { initImageUploadForm } from './upload-form/init-image-upload-form.js';
@@ -13,7 +13,7 @@ getData()
     setThumbnailsFilters(photos);
   })
   .catch(() => {
-    showDataErrorMessage();
+    showToastError();
   });
 
 initImagePopup();
